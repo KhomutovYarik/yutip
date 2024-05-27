89293d4e4ea29d6da9c83d5705ed034f49749f1e
Khomutov Yaroslav <ya.homutov@stmd.pro>
2024-05-28 04:15:32 +0500

    Removes a console.log

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-console.log(process.env.REACT_APP_PUBLIC_URL)
-
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
